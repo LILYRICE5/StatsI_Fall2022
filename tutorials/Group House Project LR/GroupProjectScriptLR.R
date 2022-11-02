fba472f02a8b1e49d015c141c814082f564a425c
LILYRICE5 <lrice@tcd.ie>
2022-11-02 13:49:36 +0000

    Create GroupProjectScriptLR.R

---
+++ tutorials/Group House Project LR/GroupProjectScriptLR.R
@@ -0,0 +1,14 @@
+##### Workings - Lily
+
+data <- read.table("https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/house_sales.csv")
+summary(data)
+head(data)
+list(data)
+
+install.packages("dplyr")
+
+separate(data, col)
+
+
+
+
